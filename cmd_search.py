c52bd58f5b19af4b23cc15d92b3b83370fe05d5d
Marc Dietrichstein <marc@dietrichstein.net>
2018-04-10 15:09:24 +0200

    Add bm25 scoring

--- cmd_search.py
+++ cmd_search.py
@@ -1,12 +1,14 @@
 from preprocessing import create_preprocessor, split_words
 from indexing import create_index_reader
-from searching import simple_tfidf_search, vecspace_tfidf_search
+from searching import simple_tfidf_search, simple_bm25_search
 
 index_filepath = 'simple.index'
 
 # search_query = 'This is my awesome search term'
 
-search_query = 'Gorbachev policy of glasnost'
+# search_query = 'Gorbachev policy of glasnost'
+
+search_query = 'Happy people are everywhere'
 
 preprocess = create_preprocessor(enable_case_folding=True,
                                  enable_remove_stop_words=True,
@@ -19,7 +21,7 @@ words = split_words(search_query,
                     strip_html_entities=True,
                     strip_square_bracket_tags=True)
 
-search_terms = preprocess(words)
+search_terms = list(preprocess(words))
 
 number_of_documents, index_reader_generator = create_index_reader(index_filepath)
 index_reader = index_reader_generator()
@@ -30,11 +32,7 @@ index = list(index_reader)
 number_of_terms = len(index)
 print('done')
 
-# document_scores = vecspace_tfidf_search(number_of_documents, index, search_terms)
-# for i in range(20):
-#   print(document_scores[i], '\n')
-
-document_scores = simple_tfidf_search(number_of_documents, index, search_terms)
+document_scores = simple_bm25_search(number_of_documents, index, search_terms)
 
 for i in range(20):
   print(document_scores[i], '\n')
--- searching.py
+++ searching.py
@@ -1,114 +1,179 @@
 import math
-import numpy as np
-from collections import namedtuple
+# import numpy as np
+from collections import namedtuple, Counter
 
 Document = namedtuple('Document', ['id', 'terms'])
 
+# def vecspace_tfidf_search(number_of_documents, index, search_terms):
+#     search_terms = list(set(search_terms))
+#     (documents, search_tokens) = __find_documents_for_terms(index, search_terms)
+#     document_scores = []
 
-def vecspace_tfidf_search(number_of_documents, index, search_terms):
-  search_terms = list(set(search_terms))
-  (documents, search_tokens) = __find_documents_for_terms(index, search_terms)
-  document_scores = []
+#     document_list = documents.values()
 
-  document_list = documents.values()
+#     number_of_search_terms = len(search_terms)
 
-  number_of_search_terms = len(search_terms)
+#     document_vectors = np.zeros((number_of_documents, number_of_search_terms))
 
-  document_vectors = np.zeros((number_of_documents, number_of_search_terms))
+#     for di, document in enumerate(document_list):
+#       document_vector = document_vectors[di]
 
-  for di, document in enumerate(document_list):
-    document_vector = document_vectors[di]
+#       for ti, search_token in enumerate(search_tokens):
 
-    for ti, search_token in enumerate(search_tokens):
+#         if search_token.term in document.terms:
+#           term_frequency = document.terms[search_token.term]
 
-      if search_token.term in document.terms:
-        term_frequency = document.terms[search_token.term]
+#         document_vector[ti] = \
+#           __tfidf_score(number_of_documents,
+#                         search_token.document_frequency,
+#                         term_frequency)
 
-      document_vector[ti] = \
-        __tfidf_score(number_of_documents,
-                      search_token.document_frequency,
-                      term_frequency)
+#         norm = np.linalg.norm(document_vector)
+#         document_vectors[di] = document_vector / norm
 
-      norm = np.linalg.norm(document_vector)
-      document_vectors[di] = document_vector / norm
+#     query_vector = np.zeros((number_of_search_terms, 1))
 
-  query_vector = np.zeros((number_of_search_terms, 1))
-
-  for ti, search_token in enumerate(search_tokens):
-    query_vector[ti] = math.log(number_of_documents / search_token.document_frequency)
+#     for ti, search_token in enumerate(search_tokens):
+#       query_vector[ti] = math.log(number_of_documents / search_token.document_frequency)
 
 
-  scores = np.dot(document_vectors, query_vector)
+#     scores = np.dot(document_vectors, query_vector)
 
-  idx = (-scores).argsort()[:50]
-  return np.array(document_list)[idx]
+#     idx = (-scores).argsort()[:50]
+#     return np.array(document_list)[idx]
+
 
 def simple_tfidf_search(number_of_documents, index, search_terms):
-  """Runs a simple tf-idf search thourgh the index
-  """
-  (documents, tokens) = __find_documents_for_terms(index, search_terms)
-  document_scores = []
+    """Runs a simple tf-idf search through the index
+    """
+    search_terms = list(set(search_terms))
 
-  document_list = documents.values()
+    (documents, tokens) = __find_documents_for_terms(index, search_terms)
+    document_scores = []
 
-  # calculate tf-idf scores
-  for document in document_list:
-    score = 0
+    document_list = documents.values()
 
-    for token in tokens:
-      term_frequency = 0
+    # calculate tf-idf scores
+    for document in document_list:
+        score = 0
 
-      if token.term in document.terms:
-        term_frequency = document.terms[token.term]
+        for token in tokens:
+            term_frequency = 0
 
-      document_frequency = token.document_frequency
+            if token.term in document.terms:
+                term_frequency = document.terms[token.term]
 
-      score += __tfidf_score(number_of_documents, document_frequency, term_frequency)
+            document_frequency = token.document_frequency
 
-    document_scores.append([document, score])
+            score += __tfidf_score(number_of_documents, document_frequency,
+                                   term_frequency)
 
-  document_scores.sort(key=lambda ds: ds[1], reverse=True)
+        document_scores.append([document, score])
 
-  return document_scores
+    document_scores.sort(key=lambda ds: ds[1], reverse=True)
+
+    return document_scores
+
+
+def simple_bm25_search(number_of_documents, index, search_terms):
+    """Runs a simple bm25 search through the index
+    """
+    (documents, tokens) = __find_documents_for_terms(index, search_terms)
+    document_scores = []
+
+    document_list = documents.values()
+
+    average_document_length = __calculate_average_document_length(document_list)
+
+    k1 = 1.2
+    b = 0.75
+    k3 = 100
+
+    search_term_counter = Counter(search_terms)
+
+    for document in document_list:
+        document_length = __document_length(document) / average_document_length
+
+        score = 0
+
+        for token in tokens:
+            term_frequency = 0
+
+            if token.term in document.terms:
+              term_frequency = document.terms[token.term]
+
+            tfq = search_term_counter[token.term]
+            tfd = term_frequency
+            dft = token.document_frequency
+
+            Bd = (1 - b) +( b * (document_length / average_document_length) )
+
+            bm25 =  (tfq / (k3 + tfq)) * (tfd / (Bd + tfd)) * math.log( (number_of_documents - dft + 0.5) / (dft - 0.5) )
+
+            score += bm25
+
+        document_scores.append([document, score])
+
+    document_scores.sort(key=lambda ds: ds[1], reverse=True)
+
+    return document_scores
+
+
+def __calculate_average_document_length(documents):
+    avg = 0
+
+    for document in documents:
+        avg += __document_length(document)
+
+    return avg / len(documents)
+
+
+def __document_length(document):
+    length = 0
+
+    for (term, count) in document.terms.items():
+        length += len(term) * count
+
+    return length
 
 
 def __tfidf_score(number_of_documents, document_frequency, term_frequency):
-  idf = math.log(number_of_documents / document_frequency)
-  return term_frequency * idf
+    idf = math.log(number_of_documents / document_frequency)
+    return term_frequency * idf
 
 
 def __find_documents_for_terms(index, search_terms):
-  """Returns matching documents and token objects for
-  the given terms
-  """
-  search_tokens = []
-  needles = set(search_terms)
+    """Returns matching documents and token objects for
+    the given terms
+    """
+    search_tokens = []
+    needles = set(search_terms)
 
-  for token in index:
-    for needle in needles:
-      if needle == token.term:
-        search_tokens.append(token)
+    for token in index:
+        for needle in needles:
+            if needle == token.term:
+                search_tokens.append(token)
 
-        remaining_needles = list(needles)
-        remaining_needles.remove(needle)
-        needles = remaining_needles
+                remaining_needles = list(needles)
+                remaining_needles.remove(needle)
+                needles = remaining_needles
 
-      if not needles:
-        break
+            if not needles:
+                break
 
-  documents = {}
+    documents = {}
 
-  # create document -> term mapping
-  for search_token in search_tokens:
-    for posting in search_token.postings:
+    # create document -> term mapping
+    for search_token in search_tokens:
+      for posting in search_token.postings:
 
-      document_id = posting[0]
-      term_frequency = posting[1]
+        document_id = posting[0]
+        term_frequency = posting[1]
 
-      if not document_id in documents:
-        documents[document_id] = Document(document_id, {})
+        if not document_id in documents:
+            documents[document_id] = Document(document_id, {})
 
-      document = documents[document_id]
-      document.terms[search_token.term] = term_frequency
+        document = documents[document_id]
+        document.terms[search_token.term] = term_frequency
 
-  return (documents, search_tokens)
+    return (documents, search_tokens)
